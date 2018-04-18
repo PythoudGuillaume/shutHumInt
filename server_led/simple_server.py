deed6bd0533beb734f20f1ceae144d756e140811
PythoudGuillaume <guillaume.pythoud@unifr.ch>
2018-04-18 14:45:01 +0200

    simple server for testing the led array

--- server_led/simple_server.py
+++ server_led/simple_server.py
@@ -1,5 +1,21 @@
-import socket
-import pickle
+import socket,pickle
+from neopixel import *
+
+# LED strip configuration:
+LED_COUNT      = 900      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+LED_ROW        = 10
+LED_COLUMN     = 90
+
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 100     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
+
+
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.bind(('', 15555))
@@ -8,15 +24,21 @@ socket.listen(5)
 client, address = socket.accept()
 print("{} connected".format( address ))
 # Create NeoPixel object with appropriate configuration.
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+## Intialize the library (must be called once before other functions).
+strip.begin()
 
 while True:
+
         try:
             response = pickle.loads(client.recv(255))
         except:
-            print("smthg wrong happen")
-
+            print('pickle error')
         if response != "":
                     print(response)
+                    for p in response:
+                        strip.setPixelColor(p,(255,255,255))
+
 
 
 
